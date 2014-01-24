5f980ed549f1963d29415eef7cac34ef106dffe1
Developer <dev@ufac.ru>
2014-01-24 23:56:00 +0400

    ---

---
+++ test/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Tyurin D <fobia3d@gmail.com>
+ */
+
+// TODO: check include path INCLUDE_PATH%
+// ini_set('include_path', ini_get('include_path'));
+
+// put your code here
+
+require_once __DIR__.'/../vendor/autoload.php';
