e194211a5a7b6219880da74c39a9734fdb98cb7d
Dmitriy Tyurin <fobia3d@gmail.com>
2014-03-13 14:29:03 +0400

    add view

--- src/view/view.php
+++ src/view/view.php
@@ -5,12 +5,20 @@
  * @author     Dmitriy Tyurin <fobia3d@gmail.com>
  * @copyright  (c) 2013, Dmitriy Tyurin
  */
+/*
+<script>
+window.JSON ||
+    document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/json3/3.2.4/json3.min.js"><\/script>');
+</script>
+<script type="text/javascript" src="/static/js/aclogger.js"></script>
+*/
 
 $i = 0;
 ?>
 <style type="text/css">
     #ac-logger-switch { position: fixed; background: #FF0000; top: 5px; left: 0px; width: 30px; height: 15px; z-index: 1000001; cursor: pointer; }
     #ac-logger { position: fixed; top: 0px; left: 0px; z-index: 1000000; font: 9px Tahoma, Geneva, sans-serif; border-bottom: 3px solid black;  height: 60%; width: 80%;   }
+    #ac-logger.hidden {display: none;}
     #ac-logger div.content { background: #CCC; overflow: auto; width: 100%; height: 100%; }
     #ac-logger thead { background: #999; }
     #ac-logger table tr { font-family: monospace; font-size: 11px; }
@@ -23,15 +31,8 @@ $i = 0;
     #ac-logger  tr .messag { color: #888a85; }
     #ac-logger  tr.error { background: rgb(255, 179, 179);  }
 </style>
-<script type="text/javascript">
-console.log(jQuery);
-    $(document).ready(function() {
-        //$("#ac-logger").resizable();
-        $("#ac-logger-switch").click(function() {
-            $("#ac-logger").toggleClass("hidden");
-        });
-        $("#ac-logger td.level:contains(error)").parent('tr').addClass('error');
-    });
+<script>
+<?php include __DIR__.'/aclogger.js'; ?>
 </script>
 <div id="ac-logger-switch">DBG</div>
 <div id="ac-logger" class="hidden">
@@ -40,22 +41,27 @@ console.log(jQuery);
             <thead>
                 <tr >
                     <th class="number"   style="width: 30px;">№</th>
-                    <th class="time"     style="width: 70px;"><?php printf("%-8s", 'time'); ?></th>
-                    <th class="level"    style="width: 85px;"><?php printf("%-9s", 'level'); ?></th>
-                    <th class="category" style="width: 70px;"><?php printf("%-9s", 'category'); ?></th>
+                    <th class="time"     style="width: 70px;"><?php printf("%-8s",
+                                                                            'time');?></th>
+                    <th class="level"    style="width: 85px;"><?php printf("%-9s",
+                                                                            'level');?></th>
+                    <th class="category" style="width: 70px;"><?php printf("%-9s",
+                                                                            'category');?></th>
                     <th class="message">message</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($Logs as $row) {?>
+<?php foreach ($Logs as $row) {?>
                     <tr>
-                        <td class="number"><?php printf("%'02d", ++ $i); ?></td>
-                        <td class="time"><?php printf("%-9s", $row['time']); ?></td>
-                        <td class="level"><?php htmlspecialchars(printf("%-9s", '[' . $row['level'] . ']')); ?></td>
-                        <td class="category"><?php htmlspecialchars(printf("%-9s", $row['ctg'])); ?></td>
-                        <td class="message"><?php echo $row['msg']; ?></td>
+                        <td class="number"><?php printf("%'02d", ++$i);?></td>
+                        <td class="time"><?php printf("%-9s", $row['time']);?></td>
+                        <td class="level"><?php htmlspecialchars(printf("%-9s",
+                                                                     '[' . $row['level'] . ']'));?></td>
+                        <td class="category"><?php htmlspecialchars(printf("%-9s",
+                                                                        $row['ctg']));?></td>
+                        <td class="message"><?php echo $row['msg'];?></td>
                     </tr>
-                <?php } ?>
+<?php }?>
             </tbody>
         </table>
     </div>
